b0f324f7c866d803777f14833fe451b1a3037ec5
Felipe Alves <felipe.alvcor@gmail.com>
2020-10-25 20:55:13 -0300

    Construcao tela inicial
    Construcao da tela inicial com os valores atuais das moedas e sua
    variacao percentual diaria.

---
+++ criptoweb/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import './styles.css';
+import '../../fonts.css';
+
+function Header() {
+    return (
+      <nav className="CriptoWeb-Header">
+        Cripto_Web
+      </nav>
+    );
+  }
+  
+  export default Header;
---
+++ criptoweb/src/pages/main/index.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import api from '../../services/api'
+
+//import {Link} from 'react-router-dom'
+
+import './styles.css';
+import '../../fonts.css';
+import '../../animations.css';
+
+export default class Main extends Component{
+
+    //Variaveis de estado
+    //necessario para conseguir utilizar as variaveis no metodo render em tempo real conforme elas sofrerem alteracoes devido ao codigo ou backend
+    state={
+        criptoInfo:[],
+        page: 1,
+    };
+
+    //Primeiro componente a ser executado dentro desse componente
+    componentDidMount(){ //componente do react
+        this.loadCriptoValues();
+    }
+
+    loadCriptoValues = async () =>{
+
+        const response = await api.get(`/api/v3/ticker/24hr`); //passando o numero da pagina (atributo estado) como parametro
+    
+        const criptoInfo=response.data
+
+        const totalCriptos=criptoInfo.length
+
+        let usdtCriptos=[]
+
+        for(let i=0;i<totalCriptos;i++){
+
+            if(criptoInfo[i].symbol.indexOf("USDT")>0){
+                usdtCriptos.push(criptoInfo[i])
+            }
+        }
+
+        this.setState({criptoInfo: usdtCriptos})
+
+        const changePercentQuery = document.querySelectorAll('.cripto-info #changePercent')
+
+        changePercentQuery.forEach(changePercentObj=>{
+
+            console.log(changePercentObj.dataset.changepercent)
+            
+            if(changePercentObj.dataset.changepercent>=0){
+                changePercentObj.style.color=`#30B673`;
+            }
+            else{
+                changePercentObj.style.color=`#EF5757`;
+            }
+
+
+        })
+    }
+
+    render(){
+        return (
+            <div className="cripto-list">
+
+                {this.state.criptoInfo.map(criptoInfo => ( //se colocar os parenteses nao precisa dar o return //estou pegando cada um dos products dessa lista
+                    <article key={criptoInfo.symbol}>
+                    
+                        <button class="cripto-info">
+                            
+                            <div id="symbol"> {criptoInfo.symbol} </div>
+                        
+                            <div id="cripto-price"> ${criptoInfo.lastPrice} </div>
+                         
+                            <div id="changePercent" data-changepercent={criptoInfo.priceChangePercent} >  {criptoInfo.priceChangePercent}% </div>
+
+                    
+                        </button>
+
+                    </article>
+                  
+                ))}     
+
+            </div>
+        );
+    }
+}
