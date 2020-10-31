ba08347d5a9540d0a0bf49bf6283a8ebccada8d3
Felipe Alves <felipe.alvcor@gmail.com>
2020-10-31 14:02:34 -0300

    Insercao de ordenadores(filtros) para as moedas
    Foi inserido um filtro para ordernar por nome, preco, ou alteracao
    percentual diaria das moedas.

--- criptoweb/src/pages/main/index.js
+++ criptoweb/src/pages/main/index.js
@@ -8,11 +8,79 @@ import './styles.css';
 import '../../fonts.css';
 import '../../animations.css';
 
+//Filter buttons
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
+
+function sortCripto(usdtCriptos,criptoOrder){
+
+    if(criptoOrder=="price"){
+        usdtCriptos=sortCriptoByPrice(usdtCriptos)
+
+    }
+    if(criptoOrder=="percentageChange"){
+        usdtCriptos=sortCriptoPercentage(usdtCriptos)
+    }
+    if(criptoOrder=="name"){
+        usdtCriptos=sortCriptoByName(usdtCriptos)
+    }
+};
+
+
+function sortCriptoByName(usdtCriptos){
+    usdtCriptos.sort((a, b) => {
+      if (a.symbol > b.symbol) {
+        return -1;
+      }
+      if (a.symbol <= b.symbol) {
+        return 1;
+      }
+    });
+};
+
+function sortCriptoByPrice(usdtCriptos){
+    usdtCriptos.sort((a, b) => {
+      if (a.lastPrice > b.lastPrice) {
+        return -1;
+      }
+      if (a.lastPrice <= b.lastPrice) {
+        return 1;
+      }
+    });
+};
+
+function sortCriptoPercentage(usdtCriptos){
+    usdtCriptos.sort((a, b) => {
+      if (a.priceChangePercent > b.priceChangePercent) {
+        return -1;
+      }
+      if (a.priceChangePercent <= b.priceChangePercent) {
+        return 1;
+      }
+    });
+
+};
+
 export default function Main(){
     //Variaveis de estado
     //necessario para conseguir utilizar as variaveis no metodo render em tempo real conforme elas sofrerem alteracoes devido ao codigo ou backend
     
     const [criptoInfo,setCriptoInfo]=useState([]);
+    const [criptoOrder, setCriptoOrder]=useState([""]);
     
     useEffect(async ()=> {
 
@@ -27,11 +95,18 @@ export default function Main(){
         for(let i=0;i<totalCriptos;i++){
 
             if(criptoInfo[i].symbol.indexOf("USDT")>0){
-                usdtCriptos.push(criptoInfo[i])
+
+                criptoInfo[i].lastPrice=parseFloat(criptoInfo[i].lastPrice);
+                criptoInfo[i].priceChangePercent=parseFloat(criptoInfo[i].priceChangePercent);
+                usdtCriptos.push(criptoInfo[i]);
             }
+
         }
 
-        setCriptoInfo(usdtCriptos);
+
+        sortCripto(usdtCriptos,criptoOrder);
+
+        setCriptoInfo(usdtCriptos.slice());
 
         const changePercentQuery = document.querySelectorAll('.cripto-info #changePercent')
 
@@ -59,32 +134,61 @@ export default function Main(){
 
         })
 
-    },[setCriptoInfo]);
+    },[setCriptoInfo,criptoOrder]);
+
+    //Filter button selectors
+    const classes = useStyles();
+
+    const handleChange = (event) => {
+        setCriptoOrder(event.target.value);
+    };
 
     return (
-        <div className="cripto-list">
 
-            {criptoInfo.map(criptoInfo => ( //se colocar os parenteses nao precisa dar o return //estou pegando cada um dos products dessa lista
-                <article key={criptoInfo.symbol}>
-                
-                    <Link to={`cripto/${criptoInfo.symbol}`} className="cripto-info animate-up" >
-
-                        <Button variant="contained" >
+        <div className="main-container">
+            <div className="cripto-filter animate-right">
+                Filters
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="filterSelection">Select your filter</InputLabel>
+                        <Select
+                        labelId="filterSelection"
+                        id="filterSelection"
+                        value={criptoOrder}
+                        onChange={handleChange}
+                        >
+                            <MenuItem value={"none"}>None</MenuItem>
+                            <MenuItem value={"price"}>Price</MenuItem>
+                            <MenuItem value={"percentageChange"}>Percentage change</MenuItem>
+                            <MenuItem value={"name"}>Name</MenuItem>
                             
-                            <div id="symbol"> {criptoInfo.symbol} </div>
-                        
-                            <div id="cripto-price"> ${criptoInfo.lastPrice} </div>
-                        
-                            <div id="changePercent" data-changepercent={criptoInfo.priceChangePercent} >  {criptoInfo.priceChangePercent}% </div>
+                        </Select>
+                   
+                </FormControl>
 
-                        </Button>
+            </div> 
+            <div className="cripto-list">
+            
+                {criptoInfo.map(criptoInfo => ( //se colocar os parenteses nao precisa dar o return //estou pegando cada um dos products dessa lista
+                    <article key={criptoInfo.symbol}>
+                    
+                        <Link to={`cripto/${criptoInfo.symbol}`} className="cripto-info animate-up" >
 
-                    </Link>
+                            <Button variant="contained" >
+                                
+                                <div id="symbol"> {criptoInfo.symbol} </div>
+                            
+                                <div id="cripto-price"> ${criptoInfo.lastPrice} </div>
+                            
+                                <div id="changePercent" data-changepercent={criptoInfo.priceChangePercent} >  {criptoInfo.priceChangePercent}% </div>
 
-                </article>
-                
-            ))}     
+                            </Button>
 
+                        </Link>
+
+                    </article>
+                    
+                ))}    
+            </div>
         </div>
     );
 
